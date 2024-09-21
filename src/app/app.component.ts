f7c216e95b4e354500aeedb3fcc4611f0b836ae7
ankithtrainer <ankith.trainer@gmail.com>
2024-09-22 03:10:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Customer } from '../cusomers';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-directive-example';
+
+   items  = [ 'Apple', 'Banana' , 'Orange']
+
+   
+customers:Customer[] = [
+  { custId:'100' , Cust_Name: 'Rahul', city:'Bangalore' , state:'Taminadu'} ,
+  { custId:'101' , Cust_Name: 'Tapan', city:'Delhi' , state:'Delhi'} 
+]
+
+readMe(){
+   console.log(" I am inside controller method ");
+}
+
+
+
 }
---
+++ src/cusomers.ts
@@ -0,0 +1,8 @@
+    
+export class Customer{
+    custId:string;
+    Cust_Name:string;
+    city:string;
+    state:string
+  
+  }
